bc2b69b263d42ac6b96286ee5e524deea2b4164f
Leonardo <leonardo.aa88@gmail.com>
2021-10-06 00:49:49 -0300

    commit

--- frontend/mynotes/src/components/ListItem.js
+++ frontend/mynotes/src/components/ListItem.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+let getTime = (note) => {
+    return new Date(note.updated).toLocaleDateString()
+}
+
+let getTitle = (note) => {
+    // split by new lines and just get the first line
+    // split will make a list of each line and will only pull on the first line
+    const title = note.body.split('\n')[0]
+    if (title.length > 45) {
+        return title.slice(0, 45)
+    }
+    return title
+}
+
+let getContent = (note) => {
+    // get content after title
+    let title = getTitle(note)
+    let content = note.body.replaceAll('\n', '') // remove space between lines
+    content = content.replaceAll(title, '') // removes title
+    if (content.length > 45) {
+        return content.slice(0, 45) + '...'
+    } else {
+        return content
+    }
+}
+
 const ListItem = ({note}) => {
     return (
         <Link to={`/note/${note.id}`}>
             <div className="notes-list-item">
-                <h3>{note.body}</h3>
+                <h3>{getTitle(note)}</h3>
+                <p><span>{getTime(note)}</span>{getContent(note)}</p>
             </div>
         </Link>
     )
