50893f767d66e09d8bf96cc7fa7c6417bacc11db
Leonardo <leonardo.aa88@gmail.com>
2021-10-05 22:20:55 -0300

    commit

--- frontend/mynotes/src/App.js
+++ frontend/mynotes/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import Body from './components/Body'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>My app</h1>
+      <Header/>
+      <Body/>
     </div>
   );
 }
